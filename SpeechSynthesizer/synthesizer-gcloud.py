f2d3149059d5f33b97fdde266ac725d05247aeb6
Cahya Wirawan <Cahya.Wirawan@gmail.com>
2021-03-23 09:48:15 +0100

    updated the synthesizer script

--- SpeechSynthesizer/synthesizer-gcloud.py
+++ SpeechSynthesizer/synthesizer-gcloud.py
@@ -1,35 +1,56 @@
-def synthesize_text(text):
-    """Synthesizes speech from the input string of text."""
-    from google.cloud import texttospeech
+import pandas as pd
+from pathlib import Path
+from google.cloud import texttospeech
 
+data_dir = Path("../data/commonvoice/id")
+output_dir = Path("../data/output")
+
+voice_types = [
+    {
+        "language_code": "id-ID",
+        "name": "id-ID-Wavenet-A",
+        "ssml_gender": texttospeech.SsmlVoiceGender.FEMALE
+    },
+    {
+        "language_code": "id-ID",
+        "name": "id-ID-Wavenet-C",
+        "ssml_gender": texttospeech.SsmlVoiceGender.MALE
+    },
+]
+
+def synthesize_text(text, file_name, voice_type):
+    """Synthesizes speech from the input string of text."""
+    path = output_dir/voice_type["name"]
+    path.mkdir(parents=True, exist_ok=True)
+    file = path/file_name
+    if file.exists():
+        return
     client = texttospeech.TextToSpeechClient()
 
     input_text = texttospeech.SynthesisInput(text=text)
 
     # Note: the voice can also be specified by name.
     # Names of voices can be retrieved with client.list_voices().
-    voice = texttospeech.VoiceSelectionParams(
-        language_code="id-ID",
-        name="id-ID-Wavenet-C",
-        ssml_gender=texttospeech.SsmlVoiceGender.MALE,
-    )
+    voice = texttospeech.VoiceSelectionParams(voice_type)
 
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.MP3
-    )
+    audio_config = texttospeech.AudioConfig({
+        "audio_encoding": texttospeech.AudioEncoding.MP3
+    })
 
-    response = client.synthesize_speech(
-        request={"input": input_text, "voice": voice, "audio_config": audio_config}
-    )
+    response = client.synthesize_speech(input=input_text, voice=voice, audio_config=audio_config)
+
 
     # The response's audio_content is binary.
-    with open("output.mp3", "wb") as out:
+    with open(path/file_name, "wb") as out:
         out.write(response.audio_content)
-        print('Audio content written to file "output.mp3"')
+        print(f'Audio content written to file {file_name}')
 
 def main():
-    text = "Sebagai pusat bisnis, politik, dan kebudayaan, Jakarta merupakan tempat berdirinya kantor-kantor pusat BUMN, perusahaan swasta, dan perusahaan asing. Kota ini juga menjadi tempat kedudukan lembaga-lembaga pemerintahan dan kantor sekretariat ASEAN."
-    synthesize_text(text)
+    df = pd.read_csv(data_dir/"validated.tsv", sep='\t', header=0)
+    for voice_type in voice_types:
+        for i, row in df.iterrows():
+            # print(row["path"], row["sentence"])
+            synthesize_text(row["sentence"], row["path"], voice_type)
 
 if __name__ == "__main__":
     main()
