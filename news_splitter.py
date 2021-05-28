99af2de306ccae51b3d0f0a204b04cd2fd100e82
Cahya Wirawan <Cahya.Wirawan@gmail.com>
2021-05-28 16:28:07 +0200

    merge short sentences

--- news_splitter.py
+++ news_splitter.py
@@ -19,12 +19,24 @@ with open(news_path, "r") as news:
                     lang = detect(line)
                     if lang == "id":
                         sentences = line.split(". ")
+                        sentence_merged = ""
                         for sentence in sentences:
                             if len(sentence) > min_text_length:
                                 sentence = tp.normalize(sentence)
+                                if len(sentence) + len(sentence_merged) < min_text_length*4:
+                                    if sentence_merged == "":
+                                        sentence_merged = sentence
+                                    else:
+                                        sentence_merged = sentence_merged + f'. {sentence}'
+                                    continue
+                                if sentence_merged == "":
+                                    sentence_merged = sentence
+                                else:
+                                    sentence_merged = sentence_merged + f'. {sentence}'
                                 if (i-1) % 10000 == 0:
-                                    print(f'newspapers_{i:09}.ogg\t{sentence}')
-                                voices.write(f'newspapers_{i:09}.ogg\t{sentence}\n')
+                                    print(f'newspapers_{i:09}.ogg\t{sentence_merged}')
+                                voices.write(f'newspapers_{i:09}.ogg\t{sentence_merged}.\n')
+                                sentence_merged = ""
                                 i += 1
                 except Exception as e:
                     print(sentence)
